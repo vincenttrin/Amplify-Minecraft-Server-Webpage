31f9d925f1ae69b8af7ffdff79ffbfd2313327f7
Vincent <vince.trinh80@gmail.com>
2024-08-15 19:19:49 -0700

    added functionality to start server button

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import axios from 'axios';
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
-
+  
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  
+  const startMinecraftServer = async () => {
+    setIsLoading(true);
+    setMessage('');
+    
+    try {
+      const minecraftLauncherApi = 'https://p23vmmnxqh.execute-api.us-east-1.amazonaws.com/default/minecraft-launcher'
 
-  function startMcServer() {
-    // insert logic to check if server is running already
-    // insert logic to  start the server
+      const response = await axios.post<APIResponse>(minecraftLauncherApi);
+
+      setMessage(response.data.message);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setMessage('An error occured while triggering the lambda function');
+        console.error('Error:', error);
+      } else {
+        setMessage('An unexpected error occurred.');
+        console.error('Unexpected error:', error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
-      <button onClick={startMcServer}>Click to start up the minecraft server</button>
+      <button onClick={startMinecraftServer} disabled={isLoading}>
+        {isLoading ? 'Starting the server...' : 'Start the Server'}
+      </button>
+      {message && <p>{message}</p>}
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.content}</li>
